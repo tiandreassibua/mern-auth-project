b552b37a9329b9f54ec7a715ccf330a1280447f6
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2023-08-19 02:10:56 +0700

    create header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <div className="bg-slate-200">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                <h1 className="font-bold">Auth App</h1>
+                <ul className="flex gap-4">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/sign-in">Sign In</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
